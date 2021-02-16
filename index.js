c068638de8b23bf4082b8a5aa302265a98a6a892
qphilippot <quentin.philippot@gmail.com>
2021-02-16 19:19:34 +0100

    export some basic exceptions

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 module.exports = {
-    STATUS_CODE: require('./src/status-code')
+    STATUS_CODE: require('./src/status-code'),
+    ExpiredTokenException: require('./src/exceptions/ExpiredToken.exception'),
+    TokenShouldBeRefreshedException: require('./src/exceptions/TokenShouldBeRefreshed.exception'),
+    InvalidTokenException: require('./src/exceptions/InvalidToken.exception'),
+    MissingIdentityTokenException: require('./src/exceptions/MissingIdentityToken.exception'),
+    MissingRefreshTokenException: require('./src/exceptions/MissingRefreshToken.exception')
 };
